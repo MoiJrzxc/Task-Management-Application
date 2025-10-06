3ff930bb88ee5195196275a259a55714401a19ac
MoiJrzxc <emieljamesescuzar415@gmail.com>
2025-10-06 09:34:27 +0800

    Added Task 2: Routing and Shell Developer work

--- Main.js
+++ Main.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// Shell Components (temporary placeholders)
+function TaskListView() {
+  return (
+    <div className="container mt-4">
+      <h2>Task List View</h2>
+      <p>This is where all tasks will be displayed.</p>
+    </div>
+  );
+}
+
+function AddTaskView() {
+  return (
+    <div className="container mt-4">
+      <h2>Add Task View</h2>
+      <p>This is where the add task form will appear.</p>
+    </div>
+  );
+}
 
 function Main() {
   return (
-    <div>
+    <Router>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Task Manager</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <Link className="navbar-brand" to="/">Task Manager</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
+
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/">Home</a>
+                <Link className="nav-link" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/add-task">Add Task</a>
+                <Link className="nav-link" to="/add-task">Add Task</Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-      {/* Add your main content here */}
-    </div>
+
+      {/* Routing Section */}
+      <Routes>
+        <Route path="/" element={<TaskListView />} />
+        <Route path="/add-task" element={<AddTaskView />} />
+      </Routes>
+    </Router>
   );
 }
 
